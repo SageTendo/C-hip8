e02f584b544adbde55a6c387a096bc1f748422a2
Nyasha Zishiri <23294558@sun.ac.za>
2024-12-20 23:14:02 +0200

    Instructions fixes

--- src/instructions.c
+++ src/instructions.c
@@ -1,13 +1,14 @@
 #include "instructions.h"
 #include "logger.h"
 
-// CLS: Clear the screen
+// 0x00E0 -> CLS: Clear the screen
 void cls(Chip8 *c8) {
   memset(c8->buffer, 0, sizeof(c8->buffer));
+  c8->draw = true;
   c8->pc += 0x2;
 }
 
-// RET: Return from a subroutine
+// 0x00EE -> RET: Return from a subroutine
 int ret(Chip8 *c8) {
   if (c8->sp <= 0) {
     log_error("Error: Stack underflow in RET instruction.");
@@ -19,12 +20,13 @@ int ret(Chip8 *c8) {
   return SUCCESS;
 }
 
-// SYS: Jump to address
+// 0x0NNN -> SYS: Jump to address
 void sys_addr(Chip8 *c8) { c8->pc = c8->opcode & 0x0FFF; }
 
-// CALL: Make a subroutine call
+// 0x2NNN -> CALL: Make a subroutine call
 int call_addr(Chip8 *c8) {
   uint16_t nnn;
+
   if ((c8->sp + 1) >= STACKSIZE) {
     log_error("Error: Stack overflow in Subroutine call.");
     return ERR;
@@ -36,10 +38,10 @@ int call_addr(Chip8 *c8) {
   return SUCCESS;
 }
 
-// JMP: Jump to address
+// 0x1NNN -> JMP: Jump to address
 void jmp_addr(Chip8 *c8) { c8->pc = c8->opcode & 0x0FFF; }
 
-// SE: Skip next instruction if Vx == kk
+// 0x3XKK -> SE: Skip next instruction if Vx == kk
 void se_vx_byte(Chip8 *c8) {
   uint8_t x, kk;
 
@@ -50,7 +52,7 @@ void se_vx_byte(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// SNE: Skip next instruction if Vx != kk
+// 0x4XKK -> SNE: Skip next instruction if Vx != kk
 void sne_vx_byte(Chip8 *c8) {
   uint8_t x, kk;
 
@@ -61,9 +63,10 @@ void sne_vx_byte(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// SE: Skip next instruction if Vx == Vy
+// 0x5XY0 -> SE: Skip next instruction if Vx == Vy
 void se_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
   if (c8->registers[x] == c8->registers[y])
@@ -71,7 +74,7 @@ void se_vx_vy(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// LD: Set Vx = kk
+// 0x6XKK -> LD: Set Vx = kk
 void ld_vx_byte(Chip8 *c8) {
   uint8_t x, kk;
 
@@ -81,7 +84,7 @@ void ld_vx_byte(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// ADD: Set Vx = Vx + kk
+// 0x7XKK -> ADD: Set Vx = Vx + kk
 void add_vx_byte(Chip8 *c8) {
   uint8_t x, kk;
 
@@ -91,46 +94,53 @@ void add_vx_byte(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// LD: Set Vx = Vy
+// 0x8XY0 -> LD: Set Vx = Vy
 void ld_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
   c8->registers[x] = c8->registers[y];
   c8->pc += 0x2;
 }
 
-// OR: Set Vx = Vx | Vy
+// 0x8XY1 -> OR: Set Vx = Vx | Vy
 void or_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
   c8->registers[x] |= c8->registers[y];
+  c8->registers[0xF] = 0;
   c8->pc += 0x2;
 }
 
-// AND: Set Vx = Vx & Vy
+// 0x8XY2 -> AND: Set Vx = Vx & Vy
 void and_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
   c8->registers[x] &= c8->registers[y];
+  c8->registers[0xF] = 0;
   c8->pc += 0x2;
 }
 
-// XOR: Set Vx = Vx ^ Vy
+// 0x8XY3 -> XOR: Set Vx = Vx ^ Vy
 void xor_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
   c8->registers[x] ^= c8->registers[y];
+  c8->registers[0xF] = 0;
   c8->pc += 0x2;
 }
 
-// ADD: Set Vx = Vx + Vy
+// 0x8XY4 -> ADD: Set Vx = Vx + Vy
 void add_vx_vy(Chip8 *c8) {
-  // FIXME:
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
 
@@ -143,10 +153,10 @@ void add_vx_vy(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// SUB: Set Vx = Vx - Vy
+// 0x8XY5 -> SUB: Set Vx = Vx - Vy
 void sub_vx_vy(Chip8 *c8) {
-  // FIXME:
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
 
@@ -155,24 +165,27 @@ void sub_vx_vy(Chip8 *c8) {
   else
     c8->registers[0xF] = 0;
 
-  c8->registers[x] = (c8->registers[x] - c8->registers[y]) & 0xFF;
+  c8->registers[x] = (c8->registers[x] - c8->registers[y]);
   c8->pc += 0x2;
 }
 
-// SHR: Set Vx = Vx >> 1
+// 0x8XY6 -> SHR: Set Vx = Vx >> 1
 void shr_vx(Chip8 *c8) {
-  uint8_t x;
+  uint8_t x, y;
 
   x = (c8->opcode & 0x0F00) >> 8;
+  y = (c8->opcode & 0x00F0) >> 4;
+
+  c8->registers[x] = c8->registers[y];
   c8->registers[0xF] = c8->registers[x] & 0x1;
   c8->registers[x] >>= 1;
   c8->pc += 0x2;
 }
 
-// SUBN: Set Vx = Vy - Vx
+// 0x8XY7 -> SUBN: Set Vx = Vy - Vx
 void subn_vx_vy(Chip8 *c8) {
-  // FIXME:
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
 
@@ -181,26 +194,27 @@ void subn_vx_vy(Chip8 *c8) {
   else
     c8->registers[0xF] = 0;
 
-  c8->registers[x] = (c8->registers[y] - c8->registers[x]) & 0xFF;
+  c8->registers[x] = (c8->registers[y] - c8->registers[x]);
   c8->pc += 0x2;
 }
 
-// SHL: Set Vx = Vx << 1
+// 0x8XYE -> SHL: Set Vx = Vx << 1
 void shl_vx(Chip8 *c8) {
-  uint8_t x;
+  uint8_t x, y;
 
   x = (c8->opcode & 0x0F00) >> 8;
-  if (c8->registers[x] & 0x80)
-    c8->registers[0xF] = 1;
-  else
-    c8->registers[0xF] = 0;
+  y = (c8->opcode & 0x00F0) >> 4;
+
+  c8->registers[x] = c8->registers[y];
+  c8->registers[0xF] = (c8->registers[x] & 0x80) >> 16;
   c8->registers[x] <<= 1;
   c8->pc += 0x2;
 }
 
-// SNE: Skip next instruction if Vx != Vy
+// 0x9XY0 -> SNE: Skip next instruction if Vx != Vy
 void sne_vx_vy(Chip8 *c8) {
   uint8_t x, y;
+
   x = (c8->opcode & 0x0F00) >> 8;
   y = (c8->opcode & 0x00F0) >> 4;
 
@@ -209,7 +223,7 @@ void sne_vx_vy(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// LD: Set I = nnn
+// 0xANNN -> LD: Set I = nnn
 void ld_i_addr(Chip8 *c8) {
   uint16_t nnn;
 
@@ -218,23 +232,25 @@ void ld_i_addr(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// JP: Jump to V0 + nnn
+// 0xBNNN -> JP: Jump to V0 + nnn
 void jp_v0_addr(Chip8 *c8) {
   uint16_t nnn;
+
   nnn = c8->opcode & 0x0FFF;
   c8->pc = nnn + c8->registers[0];
 }
 
-// RND: Set Vx = random byte AND kk
+// 0xCXKK -> RND: Set Vx = random byte AND kk
 void rnd_vx_kk(Chip8 *c8) {
   uint8_t x, kk;
+
   x = (c8->opcode & 0x0F00) >> 8;
   kk = (c8->opcode & 0x00FF);
   c8->registers[x] = (rand() & 0xFF) & kk;
   c8->pc += 0x2;
 }
 
-// DRW: Draw a sprite at (Vx, Vy)
+// 0xDXYN -> DRW: Draw a sprite at (Vx, Vy)
 void drw_vx_vy_nibble(Chip8 *c8) {
   uint8_t x, y, height, sprite, x_pos, y_pos;
 
@@ -263,25 +279,28 @@ void drw_vx_vy_nibble(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// SKP: Skip next instruction if key with the value of Vx is pressed
+// 0xEX9E SKP: Skip next instruction if key with the value of Vx is pressed
 void skp_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   if (c8->keypad[c8->registers[x]])
     c8->pc += 0x2;
   c8->pc += 0x2;
 }
 
-// SKNP: Skip next instruction if key with the value of Vx is not pressed
+// 0xEXA1 -> SKNP: Skip next instruction if key with the value of Vx is not
+// pressed
 void sknp_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   if (!c8->keypad[c8->registers[x]])
     c8->pc += 0x2;
   c8->pc += 0x2;
 }
 
-// LD: Set Vx = k (Blocking for key press)
+// 0xFX07 -> LD: Set Vx = k (Blocking for key press)
 void ld_vx_k(Chip8 *c8) {
   uint8_t x;
 
@@ -295,49 +314,55 @@ void ld_vx_k(Chip8 *c8) {
   }
 }
 
-// LD: Set Vx = delay_timer
+// 0xF0A -> LD: Set Vx = delay_timer
 void ld_vx_dt(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->registers[x] = c8->delay_timer;
   c8->pc += 0x2;
 }
 
-// LD: Set delay_timer = Vx
+// 0xFX15 -> LD: Set delay_timer = Vx
 void ld_dt_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->delay_timer = c8->registers[x];
   c8->pc += 0x2;
 }
 
-// LD: Set sound_timer = Vx
+// 0xFX18 -> LD: Set sound_timer = Vx
 void ld_st_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->sound_timer = c8->registers[x];
   c8->pc += 0x2;
 }
 
-// LD: Set I = I + Vx
+// 0xFX1E -> LD: Set I = I + Vx
 void add_i_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->IRegister += c8->registers[x];
   c8->pc += 0x2;
 }
 
-// LD: Set I = location of sprite for digit Vx
+// 0xFX29 -> LD: Set I = location of sprite for digit Vx
 void ld_f_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->IRegister = c8->registers[x] * 0x5;
   c8->pc += 0x2;
 }
 
-// LD: Store BCD representation of Vx
+// 0xFX33 -> LD: Store BCD representation of Vx
 void ld_b_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   c8->memory[c8->IRegister] = c8->registers[x] / 100;
   c8->memory[c8->IRegister + 1] = (c8->registers[x] / 10) % 10;
@@ -345,23 +370,25 @@ void ld_b_vx(Chip8 *c8) {
   c8->pc += 0x2;
 }
 
-// LD: Store registers V0 to Vx in memory
+// 0xFX55 -> LD: Store registers V0 to Vx in memory
 void ld_i_vx(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   for (int i = 0; i <= x; i++) {
-    c8->memory[c8->IRegister + i] = c8->registers[i];
+    c8->memory[c8->IRegister++] = c8->registers[i];
   }
 
   c8->pc += 0x2;
 }
 
-// LD: Load registers V0 to Vx from memory
+// 0xFX65 -> LD: Load registers V0 to Vx from memory
 void ld_vx_i(Chip8 *c8) {
   uint8_t x;
+
   x = (c8->opcode & 0x0F00) >> 8;
   for (int i = 0; i <= x; i++) {
-    c8->registers[i] = c8->memory[c8->IRegister + i];
+    c8->registers[i] = c8->memory[c8->IRegister++];
   }
 
   c8->pc += 0x2;
